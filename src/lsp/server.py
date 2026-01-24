493f858fd9e99cac8407bbb0c5873852ba156fe0
Anastasija Savic <anastasijas557@gmail.com>
2026-01-24 12:56:29 +0100

    Implemented error checking

--- src/lsp/server.py
+++ src/lsp/server.py
@@ -4,6 +4,8 @@ from pygls.server import LanguageServer
 from lsprotocol.types import (TEXT_DOCUMENT_COMPLETION, 
                               CompletionItem, CompletionList, 
                               CompletionParams)
+from lsprotocol.types import (TEXT_DOCUMENT_DID_CHANGE, TEXT_DOCUMENT_DID_OPEN,
+                              Diagnostic, Position, Range)
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from src.core.model import get_metamodel
@@ -15,6 +17,7 @@ try:
 except Exception as e:
     metamodel = None
 
+# autocomplete
 @server.feature(TEXT_DOCUMENT_COMPLETION)
 def completions(params: CompletionParams = None):
     items = []
@@ -41,6 +44,48 @@ def completions(params: CompletionParams = None):
                 items.append(CompletionItem(label=r))
 
     return CompletionList(is_incomplete=False, items=items)
+
+# error checking
+def validate(ls, params):
+    text_doc = ls.workspace.get_text_document(params.text_document.uri)
+    source = text_doc.source
+    lines = source.splitlines()
+    diagnostics = []
+
+    try:
+        metamodel.model_from_str(source)
+    except Exception as e:
+       if hasattr(e, 'line') and hasattr(e, 'col'):
+            line_idx = e.line - 1
+            col_idx = e.col - 1
+            
+            current_line = lines[line_idx] if line_idx < len(lines) else ""
+            
+            import re
+            rest_of_line = current_line[col_idx:]
+            word_match = re.match(r'^\S+', rest_of_line)
+            
+            word_length = len(word_match.group(0)) if word_match else 1
+
+            d = Diagnostic(
+                range=Range(
+                    start=Position(line=line_idx, character=col_idx),
+                    end=Position(line=line_idx, character=col_idx + word_length),
+                ),
+                message=str(e),
+                source="StoryScript"
+            )
+            diagnostics.append(d)
+
+    ls.publish_diagnostics(text_doc.uri, diagnostics)
+
+@server.feature(TEXT_DOCUMENT_DID_OPEN)
+def did_open(ls, params):
+    validate(ls, params)
+
+@server.feature(TEXT_DOCUMENT_DID_CHANGE)
+def did_change(ls, params):
+    validate(ls, params)
 
 if __name__ == "__main__":
     server.start_io()
